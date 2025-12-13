2d6424197f9144029a210d7149d93ed2e7c4e6c3
bs224028 <bs224028@sun.ac.jp>
2025-12-13 12:54:40 +0900

    fix login redirect

--- login.js
+++ login.js
@@ -2,15 +2,15 @@ async function login() {
   const id = document.getElementById("id").value;
   const pw = document.getElementById("password").value;
   const res = await fetch("data/user.json");
-  const users = await res.json();
-  const user = user.find(u => u.id === id && u.password === pw);
+  const user = await res.json();
+  const users = user.find(u => u.id === id && u.password === pw);
   console.log(users);
-  if (!user) {
+  if (!users) {
     alert("IDまたはパスワードが違います");
     return;
   }
   console.log("login success");
-  localStorage.setItem("loginUser", JSON.stringify(user));
+  localStorage.setItem("loginUser", JSON.stringify(users));
 
   window.location.href = "home.html";
 }
