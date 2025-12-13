5de7e4af8c26f0a7d205f4bd9255591334d5176f
bs224028 <bs224028@sun.ac.jp>
2025-12-13 11:16:32 +0900

    first upload

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 async function login() {
   const id = document.getElementById("userId").value;
   const pw = document.getElementById("password").value;
-  const res = await fetch("data/users.json");
+  const res = await fetch("data/user.json");
   const users = await res.json();
   const user = user.find(u => u.id === id && u.password === pw);
 
