b5395ddcd2279b360f7db6892607f470ad137efc
bs224028 <bs224028@sun.ac.jp>
2025-12-19 14:26:14 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,18 @@
 async function login() {
   const id = document.getElementById("id").value;
   const pw = document.getElementById("password").value;
-  const res = await fetch("data/user.json");
-  const user = await res.json();
-  const users = user.find(u => u.id === id && u.password === pw);
-  
-  if (!users) {
+
+  const res = await fetch("user.json");
+  const users = await res.json();
+
+  const user = users.find(u => u.id === id && u.password === pw);
+
+  if (!user) {
     alert("IDまたはパスワードが違います");
     return;
   }
-  localStorage.setItem("loginUser", JSON.stringify(users));
 
+  localStorage.setItem("loginUser", JSON.stringify(user));
   window.location.href = "home.html";
 }
 
