2728157b5c03710144fbba9db32b4f665e009217
bs224028 <bs224028@sun.ac.jp>
2025-12-22 15:22:34 +0900

    Update iken.js

--- iken.js
+++ iken.js
@@ -1,6 +1,14 @@
 function sendIken() {
   const text = document.getElementById("iken").value;
 
+  ikens.push({
+    id: Date.now(),
+    user: loginUser.username,
+    message: text,
+    date: new Date().toLocaleDateString(),
+    reply: ""
+  });
+
   if (!text) {
     alert("意見を入力してください");
     return;
