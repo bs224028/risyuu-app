fb19391df6d1f669f950422541e8fdc6b998f17d
bs224028 <bs224028@sun.ac.jp>
2025-12-22 15:55:34 +0900

    Update iken.js

--- iken.js
+++ iken.js
@@ -1,25 +1,34 @@
+const loginUser = JSON.parse(localStorage.getItem("loginUser"));
+if (!loginUser) {
+  location.href = "login.html";
+}
+
 function sendIken() {
-  const text = document.getElementById("iken").value;
-
-  ikens.push({
-    id: Date.now(),
-    user: loginUser.username,
-    message: text,
-    date: new Date().toLocaleDateString(),
-    reply: ""
-  });
-
+  const text = document.getElementById("ikenText").value.trim();
   if (!text) {
     alert("意見を入力してください");
     return;
   }
 
   const ikens = JSON.parse(localStorage.getItem("ikens")) || [];
-  ikens.push(text);
+
+  const userName =
+    loginUser.role === "管理者"
+      ? loginUser.username
+      : loginUser.id;
+
+  ikens.push({
+    id: Date.now(),
+    user: userName,
+    message: text,
+    date: new Date().toLocaleDateString(),
+    reply: ""
+  });
+
   localStorage.setItem("ikens", JSON.stringify(ikens));
 
   alert("意見を送信しました");
-  document.getElementById("iken").value = "";
+  document.getElementById("ikenText").value = "";
 }
 
 
