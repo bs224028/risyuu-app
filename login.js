fd974bd40df06a1627fa72cd27af218c354eeae7
bs224028 <bs224028@sun.ac.jp>
2025-12-26 01:55:59 +0900

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ async function login() {
   const id = document.getElementById("id").value;
   const pw = document.getElementById("password").value;
 
-  let users = JSON.parse(localStorage.getItem("users"));
+  let users = JSON.parse(localStorage.getItem("users")) || [];
 
 　if (id === "BS224028" && pw === "1118") {
  　 localStorage.setItem("loginUser", JSON.stringify({
@@ -12,11 +12,6 @@ async function login() {
   　location.href = "home.html";
  　 return;
 　}
-
-  if (!users) {
-    const res = await fetch("user.json");
-    users = await res.json();
-  }
 
   const user = users.find(u => u.id === id && u.password === pw);
 
